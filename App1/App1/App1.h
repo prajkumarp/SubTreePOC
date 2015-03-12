514899b5977f3a9545ef77a6fcf8e61c378e56b9
Panneerselvam, Rajkumar <prajkumarp@gmail.com>
2015-03-11 20:19:52 -0400

    Squashed 'ExternalFrameworks/AppComponentPOC/' content from commit 12a51d1
    git-subtree-dir: ExternalFrameworks/AppComponentPOC
    git-subtree-split: 12a51d149f7d696fd39e24de40f563cbc79111a3

---
+++ App1/App1/App1.h
@@ -0,0 +1,20 @@
+//
+//  App1.h
+//  App1
+//
+//  Created by Panneerselvam, Rajkumar on 3/11/15.
+//  Copyright (c) 2015 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for App1.
+FOUNDATION_EXPORT double App1VersionNumber;
+
+//! Project version string for App1.
+FOUNDATION_EXPORT const unsigned char App1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <App1/PublicHeader.h>
+
+#import<App1/ViewController1.h>
+#import<App1/ViewController2.h>
---
+++ App1/App1/ViewControllers/ViewController1.m
@@ -0,0 +1,38 @@
+//
+//  ViewController1.m
+//  App1
+//
+//  Created by Panneerselvam, Rajkumar on 3/11/15.
+//  Copyright (c) 2015 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import "ViewController1.h"
+
+@interface ViewController1 ()
+
+@end
+
+@implementation ViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [[self view] setBackgroundColor:[UIColor redColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
